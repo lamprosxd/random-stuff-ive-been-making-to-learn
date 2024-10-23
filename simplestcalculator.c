760cb3852a7376a1be0bda18ded37a7f3e445fea
lamprosxd <143563085+lamprosxd@users.noreply.github.com>
2024-10-23 20:53:10 +0300

    Add files via upload

---
+++ simplestcalculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int func1();
+
+int main() {
+  func1();
+  return 0;
+}
+
+int func1() {
+  double a, b, res;
+  char op;
+  int validDivision = 1;
+ scanf("%lf %c %lf", &a, &op, &b);
+
+  if (op == '*') res = a * b;
+  else if (op == '/'){if (b != 0)  res = a / b;
+  else printf("invalid division");
+      validDivision = 0;
+  }
+  else if (op == '-')res = a - b;
+  else if (op == '+')res = a + b;
+  else {printf:("error motherfucker"); }
+  if (validDivision == 1){
+  printf("%lf", res);}
+  return 0;
+}
+
